5bc344a72c6dd9c8970a41edead740080a7b8558
mmarie8002 <mmarie8002@gmail.com>
2024-10-27 09:46:33 -0400

    Jabber

---
+++ Technica/script.js
@@ -0,0 +1,6 @@
+var button1= "my-button1"
+<script>
+      document.getElementById('my-button1').addEventListener('click'), function() {
+         this.style.display = 'none'; // Hide the button
+     }
+<script/>
